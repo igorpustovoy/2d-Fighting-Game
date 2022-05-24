1d78a26ade46980455c2c7323da93043b4eee638
Igor Pustovoy <igorpustovoy1978@gmail.com>
2022-05-24 17:02:50 +0200

    basic sprite setup

--- plain_js/index.js
+++ plain_js/index.js
@@ -7,28 +7,64 @@ canvas.height = 576
 c.fillRect(0, 0, canvas.width, canvas.height);
 
 class Sprite {
-    constructor(position) {
+    constructor({ position, velocity }) {
         this.position = position
+        this.velocity = velocity
+        this.height = 150
+        this.width = 50
 
     }
 
     draw() {
         c.fillStyle = 'red';
-        c.fillRect(this.position.x, this.position.y, 50, 150);
+        c.fillRect(this.position.x, this.position.y, this.width, this.height);
+    }
+
+    update() {
+        this.draw();
+
+        this.position.y += this.velocity.y
+        if (this.position.y + this.height >= canvas.height) {
+            this.velocity.y = 0;
+        }
+
+
+        this.position.x += this.velocity.x
     }
 }
 
 const player = new Sprite({
-    x: 0,
-    y: 0
+    position: {
+        x: 0,
+        y: 0
+    },
+    velocity: {
+        x: 0,
+        y: 10
+    }
 });
 
 const enemy = new Sprite({
-    x: 400,
-    y: 100
+    position: {
+        x: 400,
+        y: 100
+    },
+    velocity: {
+        x: 0,
+        y: 10
+    }
 });
 
-player.draw();
-enemy.draw();
 
 console.log(player);
+
+function animate() {
+    window.requestAnimationFrame(animate);
+    c.clearRect(0, 0, canvas.width, canvas.height);
+    c.fillStyle = 'black';
+    c.fillRect(0, 0, canvas.width, canvas.height);
+    player.update();
+    enemy.update();
+}
+
+animate();
